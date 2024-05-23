147c0f77d41a4d269cda66601a68c82f15e5c70f
jamntia <jntiamoah22@gmail.com>
2024-05-23 13:06:10 -0400

    Complete Authentication Page
    Fully added the first page the user should see, and this should direct the user to the home page

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,10 +1,28 @@
-import { Box, VStack, Image, Input, Button } from '@chakra-ui/react';
+import { Box, VStack, Image, Input, Button, Flex, Text } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const Authform = () => {
   const [isLogin, setIsLogin] = useState(true)
-  return <>
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+  const handleAuth = () => {
+    console.log("inputs", inputs)
+    if (!inputs.email || !inputs.password) {
+      alert("Please fill all the fields");
+      return;
+    }
+
+    navigate("/");
+  };
+
+  return (
+  <>
     <Box border={"1px solid gray"} borderRadius={4} padding={5}>
       <VStack spacing={4}>
         <Image src='/logo.png' h={24} cursor={"pointer"} alt="Instagram" />
@@ -12,11 +30,15 @@ const Authform = () => {
           placeholder='Email'
           fontSize={14}
           type="email"
+          value={inputs.email}
+          onChange={(e) => setInputs({...inputs, email:e.target.value})}
         />
         <Input
           placeholder='Password'
           fontSize={14}
           type="password"
+          value={inputs.password}
+          onChange={(e) => setInputs({...inputs, password:e.target.value})}
         />
 
         {!isLogin ? (
@@ -24,15 +46,47 @@ const Authform = () => {
             placeholder='Confirm Password'
             fontSize={14}
             type="password"
+            value={inputs.confirmPassword}
+             onChange={(e) => setInputs({...inputs, confirmPassword:e.target.value})}
           />
         ) : null}
-        <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+        <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={handleAuth}>
           {isLogin ? "Log in" : "Sign Up"}
         </Button>
-      </VStack>
 
+        {/* ------------- OR ------------- */}
+        <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+          <Box flex={2} h={"1px"} bg={"gray.400"} />
+          <Text mx={1} color={"white"}>
+            OR
+          </Text>
+          <Box flex={2} h={"1px"} bg={"gray.400"} />
+        </Flex>
+
+        <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+          <Image src="/google.png" w={5} alt='Google logo' />
+          <Text mx="2" color={"blue.500"}>
+            Log in with Google
+          </Text>
+        </Flex>
+      </VStack>
     </Box>
+
+    {/* Toggle between Login and Signup */}
+    <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?": "Already have an account?"}
+          </Box>
+          <Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
+            {isLogin ? "Sign Up": "Log in"}
+          </Box>
+
+        </Flex>
+    </Box>
+
   </>
+  )
 }
 
 export default Authform
