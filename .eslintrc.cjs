42faee041755a27a437ab14c229eb2bf74984018
jamntia <jntiamoah22@gmail.com>
2024-05-27 14:23:57 -0400

    Create Sidebar
    This fixes an issue where the sidebar also showed up on the auth page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off", // added this rule to avoid prop-types error (VSCode suggestion)
   },
 }
