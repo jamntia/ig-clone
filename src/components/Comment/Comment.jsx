78f37d490c1db6b84bdd9ce5ee6f965e7e71b6b7
jamntia <jntiamoah22@gmail.com>
2024-05-28 00:31:28 -0400

    Create a Profile page
    I also added a differenct color scrollbar using CSS. The overall page looks like Instagram besides the Tagged Section

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,24 @@
+import { Flex, Avatar, Text } from "@chakra-ui/react"
+
+const Comment = ({ createdAt, username, profilePic, text }) => {
+    return (
+        <Flex gap={4}>
+            <Avatar src={profilePic} name={username} size={"sm"} />
+            <Flex direction={"column"}>
+                <Flex gap={2}>
+                    <Text fontWeight={"bold"} fontSize={12}>
+                        {username}
+                    </Text>
+                    <Text color={"gray.500"} fontSize={14}>
+                        {text}
+                    </Text>
+                </Flex>
+                <Text color={"gray"} fontSize={12}>
+                    {createdAt}
+                </Text>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Comment
--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -2,7 +2,7 @@ import { Flex, Box, Text, InputGroup, Input, InputRightElement, Button } from "@
 import { useState } from "react"
 import { NotificationsLogo, UnlikeLogo, CommentLogo } from "../../assets/constants"
 
-const PostFooter = ({username}) => {
+const PostFooter = ({ username, isProfilePage }) => {
   const [liked, setLiked] = useState(false)
   const [likes, setLikes] = useState(1000)
 
@@ -18,7 +18,7 @@ const PostFooter = ({username}) => {
   }
 
   return (
-    <Box mb={10}>
+    <Box mb={10} marginTop={"auto"}>
       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike}
           cursor={"pointer"}
@@ -34,33 +34,37 @@ const PostFooter = ({username}) => {
       <Text fontWeight={600} fontSize="sm">
         {likes} likes
       </Text>
-      <Text fontSize={"sm"} fontWeight={700}>
-        {username}{" "}
-        <Text as={"span"} fontWeight={400}>
-          Feeling good
-        </Text>
-      </Text>
-      <Text fontSize={"sm"} color={"gray"}>
-        View all 1,000 comments
-      </Text>
+      {!isProfilePage && (
+        <>
+          <Text fontSize={"sm"} fontWeight={700}>
+            {username}{" "}
+            <Text as={"span"} fontWeight={400}>
+              Feeling good
+            </Text>
+          </Text>
+          <Text fontSize={"sm"} color={"gray"}>
+            View all 1,000 comments
+          </Text></>
+      )}
+
 
       <Flex
-      alignItems={"center"}
-      gap={2}
-      justifyContent={"space-between"}
-      w={"full"}
+        alignItems={"center"}
+        gap={2}
+        justifyContent={"space-between"}
+        w={"full"}
       >
         <InputGroup>
           <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14} />
           <InputRightElement>
-          <Button
-          fontSize={14}
-          color={"blue.500"}
-          fontWeight={"600"}
-          cursor={"pointer"}
-          _hover={{color: "white"}}
-          bg={"transparent"}
-          >Post</Button>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={"600"}
+              cursor={"pointer"}
+              _hover={{ color: "white" }}
+              bg={"transparent"}
+            >Post</Button>
           </InputRightElement>
         </InputGroup>
 
