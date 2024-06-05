8455eb731f499839afb6b6c1e18d5476b0f00224
jamntia <jntiamoah22@gmail.com>
2024-06-05 02:31:00 -0400

    Create login logic
    Also changed SuggestedHeader.jsx to depend on the username instead of a predetermined path

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,11 +1,14 @@
 import { Input, Button } from "@chakra-ui/react"
 import { useState } from "react"
+import useLogin from "../../hooks/useLogin"
+import { Alert, AlertIcon } from "@chakra-ui/react"
 
 const Login = () => {
     const [inputs, setInputs] = useState({
         email: "",
         password: "",
     })
+    const { loading, error, login } = useLogin()
     return (
         <>
             <Input
@@ -25,7 +28,14 @@ const Login = () => {
                 onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
             />
 
-            <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+            {error && (
+                <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+                    <AlertIcon fontSize={12} />
+                    {error.message}
+                </Alert>
+            )}
+
+            <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} onClick={() => login(inputs)} isLoading={loading}>
                 Log in
             </Button>
         </>
--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,30 +1,38 @@
-import { Avatar, Text, Flex, Link } from "@chakra-ui/react"
-import { Link as RouterLink } from "react-router-dom"
+import { Avatar, Text, Flex, Button } from "@chakra-ui/react"
+import useLogout from "../../hooks/useLogout"
+import useAuthStore from "../../store/authStore"
+import { Link } from "react-router-dom"
 
 const SuggestedHeader = () => {
+    const { handleLogout, isLogginOut } = useLogout()
+    const authUser = useAuthStore((state) => state.user)
+    if (!authUser) return null
     return (
         <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
             <Flex alignItems={"center"} gap={2}>
-                <Avatar name="As a Programmer" size={"lg"} src="/profilepic.png" />
-                <Text fontSize={12} fontWeight={"bold"}>
-                    asaprogrammer_
-                </Text>
+                <Link to={`${authUser.username}`}>
+                    <Avatar size={"lg"} src={authUser.profeilePicURL} />
+                </Link>
+                <Link to={`${authUser.username}`}>
+                    <Text fontSize={12} fontWeight={"bold"}>
+                        {authUser.username}
+                    </Text>
+                </Link>
 
             </Flex>
-            <Link
-                as={RouterLink}
-                to={"/auth"}
+            <Button
+                size={"xs"}
+                background={"transparent"}
+                _hover={{ background: "transparent" }}
                 fontSize={14}
                 fontWeight={"medium"}
                 color={"blue.400"}
                 cursor={"pointer"}
-                style={{ textDecoration: "none" }}
-                _hover={{
-                    color: "white"
-                }}
+                onClick={handleLogout}
+                isLoading={isLogginOut}
             >
                 Log out
-            </Link>
+            </Button>
 
 
         </Flex>
