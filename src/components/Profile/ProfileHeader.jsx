f6fe8e6140f92bf1c3066c9ef1014980445be2de
jamntia <jntiamoah22@gmail.com>
2024-06-07 23:49:21 -0400

    Create follow and unfollow functions

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,13 +2,17 @@ import { Flex, AvatarGroup, Avatar, VStack, Text, Button, useDisclosure } from "
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore"
 import EditProfile from "./EditProfile"
+import useFollowUser from "../../hooks/useFollowUser"
 
 const ProfileHeader = () => {
     const { userProfile } = useUserProfileStore()
     const authUser = useAuthStore((state) => state.user)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const {isFollowing, isUpdating, handleFollowUser} = useFollowUser(userProfile?.uid)
+
     const visitingOwnProfileandAuth = authUser && authUser.username === userProfile.username
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
-    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
             <AvatarGroup
@@ -50,8 +54,11 @@ const ProfileHeader = () => {
                         alignItems={"center"}
                         justifyContent={"center"}
                     >
-                        <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }}>
-                            Follow
+                        <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }}
+                        onClick={handleFollowUser}
+                        isLoading={isUpdating}
+                        >
+                            {isFollowing ? "Unfollow" : "Follow"}
                         </Button>
                     </Flex>
                     )}
