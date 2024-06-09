e3ec798663e6ae3543458f6f8345f12439c0a74e
jamntia <jntiamoah22@gmail.com>
2024-06-08 21:47:19 -0400

    Finish suggested users functionality

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,24 +1,29 @@
 import { VStack, Flex, Text, Box, Link } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
 import SuggestedUser from "./SuggestedUser"
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers"
 
 
 const SuggestedUsers = () => {
+    const { isLoading, suggestedUsers } = useGetSuggestedUsers()
+    if (isLoading) return null
+
     return (
         <VStack py={8} px={6} gap={4}>
             <SuggestedHeader />
-            <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+            
+            {suggestedUsers.length !== 0 && (<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
                 <Text fontSize={14} fontWeight={"bold"} color={"gray.500"}>
                     Suggested for you
                 </Text>
                 <Text fontSize={14} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
                     See All
                 </Text>
-            </Flex>
+            </Flex>)}  
 
-            <SuggestedUser name="Kyrie Irving" followers={1392} avatar="https://bit.ly/dan-abramov" />
-            <SuggestedUser name="Michael Jackson" followers={567} avatar="https://bit.ly/ryan-florence" />
-            <SuggestedUser name="Renee Descartes" followers={759} avatar="https://bit.ly/code-beast" />
+            {suggestedUsers.map(user => (
+                <SuggestedUser user={user} key={user.id} />
+            ))}
 
 
             <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
