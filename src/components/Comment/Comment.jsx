158ba6558feaf7bec2ef4b015c2ddae0f90c4ff3
jamntia <jntiamoah22@gmail.com>
2024-06-13 01:11:15 -0400

    Gets Comment Details
    Returns links to users profile and timestamps

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,20 +1,29 @@
-import { Flex, Avatar, Text } from "@chakra-ui/react"
+import { Flex, Avatar, Text, Skeleton, SkeletonCircle, Link } from "@chakra-ui/react"
+import useGetUserProfileById from "../../hooks/useGetUserProfileById"
+import { timeAgo } from "../../util/timeAgo"
 
 const Comment = ({ comment }) => {
+    const { userProfile, isLoading } = useGetUserProfileById(comment.createdBy)
+    if (isLoading) return <CommentSkeleton />
+
     return (
         <Flex gap={4}>
-            {/* <Avatar src={profilePic} name={username} size={"sm"} /> */}
+            <Link to={`/${userProfile.username}`}>
+                <Avatar src={userProfile.profilePicURL} size={"sm"} />
+            </Link>
             <Flex direction={"column"}>
-                <Flex gap={2}>
-                    <Text fontWeight={"bold"} fontSize={12}>
-                        {/* {username} */}
-                    </Text>
-                    <Text color={"gray.500"} fontSize={14}>
+                <Flex gap={2} alignItems={"center"}>
+                    <Link to={`/${userProfile.username}`}>
+                        <Text fontWeight={"bold"} fontSize={12}>
+                            {userProfile.username}
+                        </Text>
+                    </Link>
+                    <Text fontSize={14}>
                         {comment.comment}
                     </Text>
                 </Flex>
                 <Text color={"gray"} fontSize={12}>
-                    {/* {createdAt} */}
+                    {timeAgo(comment.createdAt.seconds)}
                 </Text>
             </Flex>
         </Flex>
@@ -22,3 +31,15 @@ const Comment = ({ comment }) => {
 }
 
 export default Comment
+
+const CommentSkeleton = () => {
+    return (
+        <Flex gap={4} w={"full"} alignItems={"center"}>
+            <SkeletonCircle h={10} w='10' />
+            <Flex gap={1} flexDir={"column"}>
+                <Skeleton height={2} width={100} />
+                <Skeleton height={2} width={50} />
+            </Flex>
+        </Flex>
+    );
+};
